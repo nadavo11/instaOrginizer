ccac02197b35fb6b97e25871b706d5d9a4022180
nadavo11 <nadavo11@gmail.com>
2021-02-20 21:29:31 +0200

    upload works!! 😊🎈🎈�

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 from instapy_cli import client
+from instabot import Bot
 import json
 import creds
+import os
+from PIL import Image
+
 
 
 username = creds.MY_USERNAME
-password =creds.MY_PASSWORD
+password = creds.MY_PASSWORD
+text = 'rolling with the king  @luis #love #this #dog #hello_world  '
+image = 'posts/1.jpg'
+i = Image.open(image)
+i.show()
+i.resize((2000,1067),Image.ANTIALIAS)
+i.save(image,quality=95)
+bot = Bot()
+
+bot.login(username = username,password = password)
+bot.upload_photo(photo= image, caption= text)
